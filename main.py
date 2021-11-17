4d2907160e39fe31d94932d0d5d4dbf872b9dadc
MullerH <muller79924@gmail.com>
2021-11-17 12:20:23 +0800

    Add endpoint and testing: Query string

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get('/')
 async def hello_world():
     return {"Hello": "World"}
+
+
+@app.get('/query')
+async def query_name(name: str):
+    return {"name": name}
--- test/test_main.py
+++ test/test_main.py
@@ -9,3 +9,10 @@ def test_hello_world():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"Hello": "World"}
+
+
+def test_query_name():
+    name = "muller"
+    response = client.get(f"/query?name={name}")
+    assert response.status_code == 200
+    assert response.json() == {"name": name}
